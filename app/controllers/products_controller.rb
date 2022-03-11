cbafbc3ee0a602aebf0ec649c6888a8b84034e1b
Brandon Lin <bwlin93@gmail.com>
2022-03-11 15:06:33 -0600

    adds update and create functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,26 @@ class ProductsController < ApplicationController
     id = params[:id]
     render json: Product.find_by(id: id)
   end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    product.save
+  end
+
+  def update
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+
+    product.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
+  post "/products/create" => "products#create"
+  patch "/products/:id" => "products#update"
 end
