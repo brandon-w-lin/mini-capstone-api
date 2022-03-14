96c2774d897872a35722b7818b354276239dd501
Brandon Lin <bwlin93@gmail.com>
2022-03-14 12:28:16 -0500

    changes labels in the routes to better align with RESTful framework

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
-  post "/products/create" => "products#create"
+  post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
 end
