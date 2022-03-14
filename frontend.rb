8acb83ea2b4552acd69a8f65dd200da855a7ed18
Brandon Lin <bwlin93@gmail.com>
2022-03-14 11:54:20 -0500

    adds CORS

--- frontend.rb
+++ frontend.rb
@@ -1,7 +1,11 @@
 def mainmenu
-end
+  menu_main = { 1 => "See all products",
+                2 => "Find a product",
+                3 => "Create a product listing",
+                4 => "Delete a product listing" }
 
-main = { 1 => "See all products", 2 => "Find a product", 3 => "Create a product listing", 4 => "Delete a product listing" }
+  displaymenu(menu_main)
+end
 
 def displaymenu(hash)
   hash.each { |k, v| puts "#{k}. #{v}" }
